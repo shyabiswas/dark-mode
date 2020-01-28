65ed27b7211e649f83d0a823d4f163038d42e676
Shya Biswas <shyabiswas@gmail.com>
2020-01-27 19:24:37 -0500

    MVP met

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,16 +1,15 @@
-import { useState } from "react"
+import {useState}  from 'react';
 
-import { useState } from "react";
+export function useLocalStorage(key,initialValue){
+    const[storedValue, setStoredValue]=useState(()=>{
+        const item=window.localStorage.getItem(key)
+    return item ? JSON.parse(item) :initialValue;
+    });
 
-export const useLocalStorage = (key, intitialValue) => {
-  const [storedValue, setStoredValue] = useState(() => {
-    const item = window.localStorage.getItem(key);
-    return item ? JSON.parse(item) : intitialValue;
-  });
+    const setValue= value=>{
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value))
+    }
 
-  const setValue = value => {
-    setStoredValue(value);
-    window.localStorage.setItem(key, JSON.stringify(value));
-  };
-  return [storedValue, setValue];
-};
+    return [storedValue, setValue];
+}
